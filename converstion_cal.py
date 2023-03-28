996f692367534e9c810f5b80880b5f17f7cf1ecd
AdamRazo <122961294+AdamRazo@users.noreply.github.com>
2023-03-27 18:12:18 -0600

    finding out

---
+++ converstion_cal.py
@@ -0,0 +1,1 @@
+length_in_incdes
---
+++ fund_functions.py
@@ -0,0 +1,24 @@
+#function---Blokc of code that is designed
+# to perform a specidifc task
+#           built in cuntions
+#           crea5e custom funtions
+#           breas up our code into reusable parts
+def say_hello(name):
+    print("hello, " + name +"!" )
+
+    say_hello('Adam')
+
+    def add_numbers(x, y):
+        return x + y
+    def calculate_total(x, y, z):
+        total = add_numbers (x,y)
+        sum - add_numbers (x,y)
+        return sum
+    
+    result = calculate_total(3,4,5)
+    print(result)
+
+
+
+    def say hello(name="world!")
+    print()
